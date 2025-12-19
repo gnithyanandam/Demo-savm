4417c7e450c0c1fa1a66454cdf387f02de3e8031
Nithya <g.nithyanandam@hotmail.com>
2025-12-19 18:02:18 +0530

    layout changes again

--- common.js
+++ common.js
@@ -1,20 +1,15 @@
 // common.js - no HTML issues here
-fetch('_header.html')
+fetch('header.html')
   .then(response => response.text())
   .then(data => {
     var data2=document.getElementById('fsPageWrapper').innerHTML;
     document.getElementById('fsPageWrapper').innerHTML = data + data2;
   });
 
-fetch('_footer.html')
+fetch('footer.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('fsFooter').innerHTML = data;
   });
 
   
-fetch('_head.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('headSection').innerHTML = data;
-  });
